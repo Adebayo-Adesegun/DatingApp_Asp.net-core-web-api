29c1613ce519f477f85a96c81dfd16c9ce52b4f1
Adebayo Adesegun Daniel <chamsdev@outlook.com>
2019-02-23 19:01:38 +0100

    Add order by filter

--- DatingApp.Core/UserParams.cs
+++ DatingApp.Core/UserParams.cs
@@ -19,6 +19,7 @@ namespace DatingApp.Core
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99;
+        public string OrderBy { get; set; }
 
 
     }
